ccc10718d28a2cdf8fb63c9897dedebb10556774
Jason Shultz <jasshultz@gmail.com>
2015-09-22 20:42:47 -0600

    init commit

--- stopwatch-app/ViewController.swift
+++ stopwatch-app/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelTimer: UILabel!
+    
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    func result() {
+        time++
+        labelTimer.text = String(time)
+        print(time)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +32,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func resetButton(sender: AnyObject) {
+        time = 0
+        timer.invalidate()
+        labelTimer.text = "0"
+    }
 
 }
 
